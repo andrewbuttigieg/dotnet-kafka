bc763be70789c53a09e43917bc2195dd390a3040
Andrew Buttigieg <andrewbuttigieg@gmail.com>
2018-09-21 17:25:05 +0200

    Producer consumer example with docker

---
+++ Publisher/Program.cs
@@ -0,0 +1,36 @@
+﻿
+  using System;
+  using System.Collections.Generic;
+  using System.Text;
+  using Confluent.Kafka;
+  using Confluent.Kafka.Serialization;
+
+namespace dotnet_kafka
+{
+    
+  public class Program
+  {
+    static void Main(string[] args)
+    {
+      var config = new Dictionary<string, object>
+      {
+        { "bootstrap.servers", "localhost:9092" }
+      };
+
+      using (var producer = new Producer<Null, string>(config, null, new StringSerializer(Encoding.UTF8)))
+      {
+        string text = null;
+
+        while (text != "exit")
+        {
+            Console.WriteLine("Type a phrase and press [Enter] to publish:");
+          text = Console.ReadLine();                          
+          Console.WriteLine($"Publishing {text}");                                                                                                                                    
+          producer.ProduceAsync("hello-topic", null, text);
+        }
+
+        producer.Flush(100);
+      }
+    }
+  }
+}
